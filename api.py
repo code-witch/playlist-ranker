cd2bdfc9f15b537b01b0c41cdd997609d5639832
code-witch <38577792+code-witch@users.noreply.github.com>
2023-10-29 00:39:06 -0600

    fix song data

--- api.py
+++ api.py
@@ -8,15 +8,31 @@ class API:
         self.sc = spotify_client
         self.playlist = [] 
         self.spotify_playlist = []
+        self.current_song = None
+        self.position = 0
 
-    def load_json(self,path):
+    def load_json(self,path,encoding='utf-8'):
         with open(path,'r') as file:
             f = file.read()
             self.playlist = json.loads(f)
 
-    def save_json(self,path):
+    def save_json(self,path,encoding='utf-8'):
         with open(path, 'w') as file:
             file.write(json.dumps(self.playlist,indent=4))
+
+    def set_current_song(self):
+        if self.current_song == None:
+            self.current_song = self.get_next_song()
+        elif self.current_song['category'] != None:
+            self.position += 1
+            self.current_song = self.get_next_song()
+
+    def get_next_song(self):
+        for i in range(self.position,len(self.playlist)):
+            if self.playlist[i]['category'] == None:
+                self.position = i
+                return self.playlist[i]
+
 
     def get_entire_spotify_playlist(self,playlist_id):
         offset = 0
@@ -40,42 +56,31 @@ class API:
             print(song)
 
         self.shuffle()
-        # might not need to return it if just setting it
-        return self.playlist 
 
 
     def convert_spotify_format_to_different_format(self,song):
         position = 0
+        excluded_artists = [
+        '88rising',
+        'thefatrat',
+        'daedo',
+        'just b',
+        'lady gaga',
+        'rich brian',
+        'dua lipa',
+        'anne-marie',
+        'yellow claw'
+        ] # 7nl5BnMM9PZNSUXuJvm1b2
         if len(song['artists']) > 1:
             for i in range(0,len(song['artists'])):
-                pass
-                # put these in a hash? check over them? or just use if?
-                # 88rising
-                # thefatrat
-                # daedo
-                # just b
-                # lady gaga
-                # rich brian
-                # dua lipa
-                # anne-marie
-                # yellow claw
-                
-                # "".replace(' ', '').lower()
-                # if any of those
-                #   position += 1
-                # else
-                #   break 
+                for artist in excluded_artists:
+                    if artist.replace(' ','').lower() == song['artists'][position]['name'].replace(' ','').lower():
+                        position += 1
 
-                # TODO
-                # maybe check if first artist doesnt match like 88rising or something
-                # so it doesnt find like bibi as a feature when theres more than 2
-                # find correct artists
-                # maybe use album artists? 
+        self.playlist.append(Song(song['uri'],song['name'],song['artists'][position]['name'],song['album']['name']).__dict__)
 
-        self.playlist.append(Song(song['uri'],song['name'],song['artists'][position],song['album']['name']).__dict__)
-
-    def play_song(self,song):
-        self.sc.start_playback(uris=song.uri)
+    def play_song(self,uri=None):
+        self.sc.start_playback(uris=uri)
 
     def stop_song(self):
         self.sc.pause_playback()
