99c6362d00738bad2a26fb9e13762877af30e5dc
code-witch <38577792+code-witch@users.noreply.github.com>
2023-11-16 00:31:14 -0700

    remove comments

--- category.py
+++ category.py
@@ -1,16 +1,4 @@
 class Category:
-    # Perfect 10
-    # Almost Perfect 9
-    # Very Good 7 
-    # Good 6
-    
-    # Mid 3 
-    # Havent heard Surprisingly Good 2
-    # Havent heard Good 1
-    # Bad 0
-    # Havent heard Not add worthy -1
-    # Havent Heard Bad -2
-
 
     def __init__(self, description: str, rank: int):
         self.description = description
