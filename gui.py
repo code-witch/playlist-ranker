e690dbb724c0d1b28832c8d4ec9a36afac87c935
code-witch <38577792+code-witch@users.noreply.github.com>
2023-11-16 00:35:36 -0700

    simplify code

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 from category import Category
-# from tkinter import ttk
+from tkinter import ttk
 
 import json
 
@@ -30,11 +30,12 @@ class GUI:
         # Buttons
         self.play_button = tk.Button(GUI.root, text='Play Song', command=lambda: print('play')) # plays current song
         self.pause_button = tk.Button(GUI.root, text='Pause Song', command=lambda: print('pause')) # pauses music
-        self.submit_button = tk.Button(GUI.root, text='Submit', command=lambda: print('submit')) # gets next song
+        self.submit_button = tk.Button(GUI.root, text='Submit', command=lambda: self.submit_data()) # gets next song
 
         # Misc
         self.notes = tk.Text(GUI.root, height=4, width=40)
-
+        self.option_dropdown = ttk.Combobox(GUI.root,values=self.api.categories)
+        self.option_dropdown.set(self.api.categories[-1])
 
     def style(self):
         pass
@@ -42,8 +43,7 @@ class GUI:
     def start(self):
         self.init_widgets()
         self.menus()
-        self.song_info(None,None)
-        self.categories()
+        # self.song_info(None,None)
         self.style()
         GUI.root.config(menu=self.menu_bar)
         GUI.root.mainloop()
@@ -69,44 +69,40 @@ class GUI:
 
         self.menu_bar.add_cascade(label='Guess Mode', menu=self.guess_menu)
 
-    def categories(self):
-        options = [
-            Category('1',0),
-            Category('2',1),
-            Category('3',2),
-            Category('4',3),
-            Category('5',4),
-        ]
-        option_dropdown = tk.OptionMenu(GUI.root, 'test', options)
-        option_dropdown.pack()
 
     def player(self):
         pass
 
     def save(self):
-        # TODO only select .json
-        GUI.file_path = filedialog.asksaveasfilename(initialdir='.')
+        GUI.file_path = filedialog.asksaveasfilename(initialdir='.', filetypes=(('Json File', '*.json'),))
         print(GUI.file_path)
         GUI.api.save_json(GUI.file_path)
 
 
     def load(self):
-        # TODO only select .json
-        GUI.file_path = filedialog.askopenfilename(initialdir='.')
+        GUI.file_path = filedialog.askopenfilename(initialdir='.',filetypes=(('Json File', '*.json'),))
         print(GUI.file_path)
         GUI.api.load_json(GUI.file_path)
         for song in GUI.api.playlist:
             print(song)
+
+        self.api.current_song = None
+        self.api.set_current_song()
+        print("CURRENT SONG", self.api.current_song)
         # print(GUI.api.playlist)
 
-    def song_info(self, title, artist):
-        # TODO maybe instead of passing it in, get it from api?
-        self.title['text'] = f'Title: {title}'
-        self.artist['text'] = f'Artist: {artist}'
+    def song_info(self):
+        self.title['text'] = f'Title: {self.api.current_song["title"]}'
+        self.artist['text'] = f'Artist: {self.api.current_song["artist"]}'
         self.title.pack()
         self.artist.pack()
 
-
+    def submit_data(self):
+        title = self.title_entry.get()
+        artist = self.artist_entry.get()
+        category = self.api.get_category_by_name(self.option_dropdown.get()).__dict__
+        
+        print(f'{title} by {artist} Rank: {category}')
 
     def load_spotify_playlist_popup(self):
         popup = tk.Toplevel(GUI.root)
@@ -128,9 +124,12 @@ class GUI:
         self.title_entry.pack()
         self.artist_guess.pack()
         self.artist_entry.pack()
+        self.title['text'] = 'Title: Secret'
+        self.artist['text'] = 'Artist: Secret'
         
     def disable_guess_boxes(self):
         self.title_guess.pack_forget()
         self.title_entry.pack_forget()
         self.artist_guess.pack_forget()
         self.artist_entry.pack_forget()
+        self.song_info()
