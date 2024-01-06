4f4427df849930891f436cb57de8c10260c86bbe
code-witch <38577792+code-witch@users.noreply.github.com>
2024-01-05 23:04:27 -0700

    complete?

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import json
 import random
+
 from song import Song
 from category import Category
 
@@ -16,7 +17,7 @@ class API:
             Category('Very Good', 8),
             Category('Good', 7),
             Category('Fine', 6),
-            Category('PLACE HOLDER CHANGE ME', 5),
+            Category('Mediocre', 5),
             Category('Boring', 4),
             Category('Havent Heard and Very Good', 3),
             Category('Havent Heard and Good', 2),
@@ -25,23 +26,41 @@ class API:
         ]
 
     def load_json(self,path):
+        self.playlist = []
         with open(path,'r',encoding='utf-8') as file:
-            self.playlist = json.loads(file.read())
+            temp_json = json.loads(file.read())
+            for song in temp_json:
+                if song['category'] == None:
+                    category = None
+                else:
+                    category = self.get_category_by_name(song['category']['description'])
+
+                self.playlist.append(Song(song['uri'],song['title'],song['artist'],song['album'], category, song['title_guess'], song['artist_guess'], song['notes']))
 
     def save_json(self,path):
         with open(path, 'w', encoding='utf-8') as file:
-            file.write(json.dumps(self.playlist,indent=4, ensure_ascii=False))
+            temp_json = []
+            for song in self.playlist:
+                if song.category == None:
+                    category = None
+                else:
+                    category = song.category.__dict__
+                temp_json.append(song)
+                temp_json[-1].category = category
+                temp_json[-1] = temp_json[-1].__dict__
+            
+            file.write(json.dumps(temp_json,indent=4, ensure_ascii=False))
 
     def set_current_song(self):
         if self.current_song == None:
             self.current_song = self.get_next_song()
-        elif self.current_song['category'] != None:
+        elif self.current_song.category != None:
             self.position += 1
             self.current_song = self.get_next_song()
 
     def get_next_song(self):
         for i in range(self.position,len(self.playlist)):
-            if self.playlist[i]['category'] == None:
+            if self.playlist[i].category == None:
                 self.position = i
                 return self.playlist[i]
 
@@ -50,6 +69,12 @@ class API:
             if category.description.lower() == name.lower():
                 return category
         return None
+
+    def update_song(self,updated_song):
+        for i in range(0,len(self.playlist)):
+            if self.playlist[i].uri == updated_song.uri:
+                self.playlist[i] = updated_song
+                break
 
     def get_entire_spotify_playlist(self,playlist_id):
         offset = 0
@@ -67,13 +92,11 @@ class API:
                 offset += total - offset
                 continue
             break
-
+        self.playlist = []
         for song in self.spotify_playlist:
-            self.convert_spotify_format_to_different_format(song)
-            # print(song)
+            self.playlist.append(self.convert_spotify_format_to_different_format(song))
 
         self.shuffle()
-
 
     def convert_spotify_format_to_different_format(self,song):
         index = 0
@@ -93,13 +116,13 @@ class API:
                 for artist in excluded_artists:
                     if artist.replace(' ','').lower() == song['artists'][index]['name'].replace(' ','').lower():
                         index += 1
-
-        self.playlist.append(Song(song['uri'],song['name'],song['artists'][index]['name'],song['album']['name']).__dict__)
+        return Song(song['uri'],song['name'],song['artists'][index]['name'],song['album']['name'])
 
     def play_song(self,uri=None):
         self.sc.start_playback(uris=uri)
 
     def stop_song(self):
+        # TODO maybe add var to key track of play time
         self.sc.pause_playback()
 
     def shuffle(self):
--- gui.py
+++ gui.py
@@ -1,9 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
-from category import Category
-from tkinter import ttk
-
-import json
+from tkinter import ttk, filedialog
 
 class GUI:
     root = None
@@ -14,36 +10,48 @@ class GUI:
         GUI.api = api
         GUI.root = tk.Tk()
         GUI.root.title('playlist ranker')
-        GUI.root.geometry('500x500')
+        # GUI.root.geometry('500x500')
 
     def init_widgets(self):
-        # song info
+        # Song Info
         self.title = tk.Label(GUI.root, text='Title:')
         self.artist = tk.Label(GUI.root, text='Artist:')
 
         # Guessing
-        self.title_guess = tk.Label(GUI.root, text='Title:')
+        self.title_guess = tk.Label(GUI.root, text='Guess Title:')
         self.title_entry = tk.Entry(GUI.root)
-        self.artist_guess = tk.Label(GUI.root, text='Artist:')
+        self.artist_guess = tk.Label(GUI.root, text='Guess Artist:')
         self.artist_entry = tk.Entry(GUI.root)
 
         # Buttons
-        self.play_button = tk.Button(GUI.root, text='Play Song', command=lambda: print('play')) # plays current song
-        self.pause_button = tk.Button(GUI.root, text='Pause Song', command=lambda: print('pause')) # pauses music
-        self.submit_button = tk.Button(GUI.root, text='Submit', command=lambda: self.submit_data()) # gets next song
+        self.play_button = tk.Button(GUI.root, text='Play', width=10, command=lambda: GUI.api.play_song([GUI.api.current_song.uri])) # plays current song
+        self.pause_button = tk.Button(GUI.root, text='Pause', width=10, command=lambda: GUI.api.stop_song()) # pauses music
+        self.submit_button = tk.Button(GUI.root, text='Submit', width=10, command=lambda: self.submit_data()) # gets next song
 
         # Misc
+        self.notes_label = tk.Label(GUI.root, text='Extra Notes')
         self.notes = tk.Text(GUI.root, height=4, width=40)
-        self.option_dropdown = ttk.Combobox(GUI.root,values=self.api.categories)
+        self.option_dropdown = ttk.Combobox(GUI.root,values=self.api.categories,width=30)
         self.option_dropdown.set(self.api.categories[-1])
 
     def style(self):
         pass
 
+    def position_widgets(self):
+        self.title.grid(column=0, row=0)
+        self.artist.grid(column=0,row=1)
+        self.enable_guess_boxes()
+        self.option_dropdown.grid(columnspan=3, column=0, row=2)
+        self.notes_label.grid(column=0, row=3)
+        self.notes.grid(columnspan=3, column=0, row=4)
+        self.play_button.grid(column=0, row=5)
+        self.pause_button.grid(column=1, row=5)
+        self.submit_button.grid(column=2, row=5)
+
     def start(self):
         self.init_widgets()
         self.menus()
-        # self.song_info(None,None)
+        self.position_widgets()
         self.style()
         GUI.root.config(menu=self.menu_bar)
         GUI.root.mainloop()
@@ -59,7 +67,7 @@ class GUI:
 
         self.spotify_menu = tk.Menu(self.menu_bar, tearoff=0)
         self.spotify_menu.add_command(label='Load Playlist...', command = lambda: self.load_spotify_playlist_popup())
-        self.spotify_menu.add_command(label='Create Playlist...', command =  lambda: print('create playlist'))
+        self.spotify_menu.add_command(label='Create Playlist...', command =  lambda: self.create_playlist())
 
         self.menu_bar.add_cascade(label='Spotify', menu=self.spotify_menu)
 
@@ -69,40 +77,64 @@ class GUI:
 
         self.menu_bar.add_cascade(label='Guess Mode', menu=self.guess_menu)
 
-
-    def player(self):
-        pass
-
     def save(self):
         GUI.file_path = filedialog.asksaveasfilename(initialdir='.', filetypes=(('Json File', '*.json'),))
-        print(GUI.file_path)
+        print('Saving file at:', GUI.file_path)
         GUI.api.save_json(GUI.file_path)
-
 
     def load(self):
         GUI.file_path = filedialog.askopenfilename(initialdir='.',filetypes=(('Json File', '*.json'),))
-        print(GUI.file_path)
+        print('Loading file from:', GUI.file_path)
         GUI.api.load_json(GUI.file_path)
         for song in GUI.api.playlist:
             print(song)
 
-        self.api.current_song = None
-        self.api.set_current_song()
-        print("CURRENT SONG", self.api.current_song)
-        # print(GUI.api.playlist)
+        GUI.api.current_song = None
+        GUI.api.set_current_song()
+        # print("CURRENT SONG", self.api.current_song)
 
     def song_info(self):
-        self.title['text'] = f'Title: {self.api.current_song["title"]}'
-        self.artist['text'] = f'Artist: {self.api.current_song["artist"]}'
-        self.title.pack()
-        self.artist.pack()
+        if GUI.api.current_song == None:
+            return
+        self.title['text'] = f'Title: {GUI.api.current_song.title}'
+        self.artist['text'] = f'Artist: {GUI.api.current_song.artist}'
 
     def submit_data(self):
+        # get GUI data
         title = self.title_entry.get()
         artist = self.artist_entry.get()
-        category = self.api.get_category_by_name(self.option_dropdown.get()).__dict__
+        category = GUI.api.get_category_by_name(self.option_dropdown.get())
+        notes = self.notes.get('1.0','end-1c').replace('\n', ' | ')
         
-        print(f'{title} by {artist} Rank: {category}')
+        # set new data 
+        GUI.api.current_song.category = category
+        GUI.api.current_song.title_guess = title
+        GUI.api.current_song.artist_guess = artist
+        GUI.api.current_song.notes = notes
+
+        # update everything
+        GUI.api.update_song(GUI.api.current_song)
+        print(GUI.api.current_song)
+        print(f'{title} by {artist} Rank: {category.__dict__}')
+        self.add_song_to_temp_file(GUI.api.current_song)
+        
+        # change song 
+        GUI.api.set_current_song()
+        print(GUI.api.current_song)
+
+        # clear guess entries after submitting
+        self.title_entry.delete(0, 'end')
+        self.artist_entry.delete(0, 'end')
+        self.notes.delete('1.0','end')
+
+    def create_playlist(self):
+        print('create playlist')
+        # TODO order songs in category rank order
+        # dont add any with category.rank <= 1
+
+    def add_song_to_temp_file(self, song):
+        with open('.temp', 'a', encoding='utf-8') as file:
+            file.write(str(song) + '\n')
 
     def load_spotify_playlist_popup(self):
         popup = tk.Toplevel(GUI.root)
@@ -117,19 +149,20 @@ class GUI:
         popup.destroy()
         GUI.api.get_entire_spotify_playlist(playlist_id)
 
-
-
     def enable_guess_boxes(self):
-        self.title_guess.pack()
-        self.title_entry.pack()
-        self.artist_guess.pack()
-        self.artist_entry.pack()
+        self.title_guess.grid(column=1,row=0)
+        self.title_entry.grid(column=2,row=0)
+        self.artist_guess.grid(column=1,row=1)
+        self.artist_entry.grid(column=2,row=1)
         self.title['text'] = 'Title: Secret'
         self.artist['text'] = 'Artist: Secret'
         
     def disable_guess_boxes(self):
-        self.title_guess.pack_forget()
-        self.title_entry.pack_forget()
-        self.artist_guess.pack_forget()
-        self.artist_entry.pack_forget()
+        self.title_guess.grid_forget()
+        self.title_entry.grid_forget()
+        self.title_entry.delete(0, 'end')
+
+        self.artist_guess.grid_forget()
+        self.artist_entry.grid_forget()
+        self.artist_entry.delete(0, 'end')
         self.song_info()
